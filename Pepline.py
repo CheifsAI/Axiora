d96c7db12955b146e79c36c007ebb2387ecc1903
MINAMOREED <94475420+MINAMOREED@users.noreply.github.com>
2024-11-25 16:23:57 +0200

    Update Pepline.py

--- Pepline.py
+++ Pepline.py
@@ -2,5 +2,5 @@ from Main import summerizer, nulldrop
 from OprFuncs import csvpd
 from Models import llama3b, codem
 df = csvpd("WorldCupMatches.csv")
-# summerizer(df,llama3b)
+summerizer(df,llama3b)
 nulldrop(df,llama3b)
