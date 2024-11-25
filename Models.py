cec684e79cdab70941064c2769adafd28b99ef3e
Hussam ElNemr <hassom20042019@gmail.com>
2024-11-25 12:24:19 +0200

    OllamaLLM

--- Models.py
+++ Models.py
@@ -1,3 +1,3 @@
-from langchain_community.llms import Ollama
-llama3b = Ollama(model="llama3.2:3b")
-codem = Ollama(model="granite-code:3b")
+from langchain_ollama import OllamaLLM
+llama3b = OllamaLLM(model="llama3.2:3b")
+codem = OllamaLLM(model="granite-code:3b")
