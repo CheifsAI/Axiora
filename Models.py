0ab28bbcdefb7fe3c028f8f94f0c7904da345a92
Hussam ElNemr <hassom20042019@gmail.com>
2024-11-25 16:33:16 +0200

    Test datasets

--- Models.py
+++ Models.py
@@ -1,3 +1,3 @@
 from langchain_ollama import OllamaLLM
 llama3b = OllamaLLM(model="llama3.2:3b")
-codem = OllamaLLM(model="granite-code:3b")
+granite_code3b = OllamaLLM(model="granite-code:3b")
--- Pepline.py
+++ Pepline.py
@@ -1,6 +1,6 @@
 from Main import summerizer, nulldrop
 from OprFuncs import csvpd
-from Models import llama3b, codem
+from Models import llama3b
 df = csvpd("WorldCupMatches.csv")
 summerizer(df,llama3b)
 nulldrop(df,llama3b)
