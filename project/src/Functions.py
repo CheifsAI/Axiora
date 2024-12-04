fb875fed20082ecc7db599b02b4c57f55f6532c6
MINAMOREED <minamoreed68@gmail.com>
2024-12-04 23:19:40 +0200

    Functions

--- project/src/Functions.py
+++ project/src/Functions.py
@@ -1,5 +1,22 @@
 #GUI Functions
+
+from Custom_Widgets import *
+from Custom_Widgets.QAppSettings import QAppSettings
+from Custom_Widgets.QCustomTipOverlay import QCustomTipOverlay
+from Custom_Widgets.QCustomLoadingIndicators import QCustomLoadingIndicators
+
+from Pyside6.QtCore import QSettings, QTimer
+from PySide6.QtGui import QColor, QFont, QFontDatabase
+from PySide6.QtWidgets import QGraphicsDropShadowEffect
+
+
+
+
 class GuiFunctions():
     def__init__(sef,MainWindow):
         self.main = MainWindow
         self.ui = MainWindow.ui
+    
+    
+    #initialize app theme
+    
