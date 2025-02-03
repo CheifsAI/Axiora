8c784a2cde1f09a6e1421d3a20b6aa61b3492090
Hussam ElNemr <hassom20042019@gmail.com>
2025-02-03 16:48:08 +0200

    clean

--- Models.py
+++
@@ -1,5 +0,0 @@
-from langchain_ollama import OllamaLLM
-llama3b = OllamaLLM(model="llama3.2:3b")
-granite_code3b = OllamaLLM(model="granite-code:3b")
-starcoder2 = OllamaLLM(model="starcoder2:3b")
-deepseek7b = OllamaLLM(model="deepseek-r1:7b")
--- Pepline.py
+++
@@ -1,8 +0,0 @@
-from Main import summerizer, nulldrop
-from OprFuncs import csvpd
-from Main import drop_nulls
-from Models import llama3b
-from Memory import memory
-df = csvpd("Test_Datasets\WorldCupMatches.csv")
-#summerizer(df,llama3b)
-drop_nulls(df,llama3b)
