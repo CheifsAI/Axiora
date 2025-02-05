f684ebff4b23ff4d738c94db8e8b2bd08706ee4d
Hussam ElNemr <hassom20042019@gmail.com>
2025-02-05 22:24:55 +0200

    bs

--- Functions.py
+++ Functions.py
@@ -17,13 +17,14 @@ class GuiFunctions():
         self.main_window = MainWindow
         self.ui = MainWindow.ui
         self.llm = llama3b
-        
         self.setup_connections()
+
     def setup_connections(self):
          self.main_window.ui.openfile_btn.clicked.connect(self.handle_data_button)
          self.main_window.ui.sum_btn.clicked.connect(self.handle_sum_btn)
          self.main_window.ui.btn_LLMs.clicked.connect(self.handle_btn_LLMs)
          self.main_window.ui.clean_data_btn.clicked.connect(self.handle_clean_data_btn)
+         self.main_window.ui.qu_btn.clicked.connect(self.handle_qu_btn)
 
     def handle_data_button(self):
         fpath, _ = QFileDialog.getOpenFileName(
@@ -83,3 +84,5 @@ class GuiFunctions():
 # result = quetions_gen(llm=llm,dataframe=df1,num=2)
 # for i, question in enumerate(result, 1):
 #    print(markdown(question))
+    #def handle_qu_btn(self,self.num_qu):
+     #   print("I'm Clicked")
