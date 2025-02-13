0abde486ca6b4d0d9dcdd9f25f13face5f7a9a11
marwan-14 <marwanelraseef.14@gmail.com>
2025-02-13 22:49:53 +0200

    Update first user.sql

--- first user.sql
+++ first user.sql
@@ -1,5 +1,5 @@
 use axiora ; 
 
-INSERT INTO users (username, password,email) VALUES ('cheif','1234',"chiefs@gmail.com");	
+INSERT INTO users (username, password,email) VALUES ('maro','202425',"maro@gmail.com");	
 
 select * from users;
