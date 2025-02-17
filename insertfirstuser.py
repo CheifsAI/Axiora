41c7490e4458202ba8c152770e8c8f5a8a724891
Hussam ElNemr <hassom20042019@gmail.com>
2025-02-18 00:08:58 +0200

    Update insertfirstuser.py

--- insertfirstuser.py
+++ insertfirstuser.py
@@ -4,10 +4,18 @@ from Axioradb import engine,users
 Session = sessionmaker(bind=engine)
 session = Session()
 
+maro = users(username='maro', email='maro@gmail.com', password='202425')
+cheif = users(username='cheif', email='cheifs@gmail.com', password='12345')
+
+session.add(maro)
+session.add(cheif)
+
+session.commit()
+
 users_list = session.query(users).all()
 
 # Print the users
 for user in users_list:
     print(user)
-
+    
 session.close()
