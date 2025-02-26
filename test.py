e665b8e34dd19b76ea6af1e5ed46bb5bca54ac05
Hussam ElNemr <hassom20042019@gmail.com>
2025-02-26 18:06:13 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from DataAnalyzer import DataAnalyzer
+from Models import llama3b
+df = pd.read_csv("Test_Datasets/WorldCupMatches.csv")
+questions = ["Who is the most teams played as home team?","Who is the most teams played as away team?"]
+analyzer = DataAnalyzer(llm=llama3b,dataframe=df)
+agentcode = analyzer.visual(questions_list=questions)
+for code in agentcode:
+    exec(code)
+    print(f"{code}\n\n\n\n\n\n\n")
