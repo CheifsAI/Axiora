36fcd1e7958a9a942504365dcb4638a9c8f28e4b
Hussam ElNemr <hassom20042019@gmail.com>
2025-03-04 01:18:31 +0200

    Update

--- insert_user.py
+++ insert_user.py
@@ -4,11 +4,7 @@ from Axioradb import engine,users,LLM
 Session = sessionmaker(bind=engine)
 session = Session()
 
-llama3b = LLM(llm_name='llama3.2:3b', parameters='3', install_llm_code='ollama run llama3.2')
 
-session.add(llama3b)
-
-session.commit()
 
 llm_list = session.query(LLM).all()
 
