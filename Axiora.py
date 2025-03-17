51f19b465037ad6987399f0d0260f80ffadc8065
Hussam ElNemr <hassom20042019@gmail.com>
2025-03-17 23:04:57 +0200

    Linux/Windows

--- Axiora.py
+++ Axiora.py
@@ -36,7 +36,10 @@ class MainWindow(QMainWindow):
         
         # USE CUSTOM TITLE BAR | USE AS "False" FOR MAC OR LINUX
         # ///////////////////////////////////////////////////////////////
-        Settings.ENABLE_CUSTOM_TITLE_BAR = True
+        if platform.system() == "Windows":
+            Settings.ENABLE_CUSTOM_TITLE_BAR = True
+        else:
+            Settings.ENABLE_CUSTOM_TITLE_BAR = False
 
         # APP NAME
         # ///////////////////////////////////////////////////////////////
